2092a2448af5386a9d283dac753f2598862c1b05
bilguunerkh.m <bilguunerkh.m@nomin.net>
2024-08-06 20:35:00 +0800

    add to facebook linl and arcoin lyric

--- pages/questions/payment.js
+++ pages/questions/payment.js
@@ -26,6 +26,14 @@ const Payment = () => {
   };
 
   // Instagram link gradient style
+  const facebookLinkStyle = {
+    background: 'linear-gradient(to right, #fff, #0064e0, #0082fb)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    textDecoration: 'underline', // Add underline for visibility
+  };
+
+  //Facebook link gradient style
   const instagramLinkStyle = {
     background: 'linear-gradient(to right, #f58529, #dd2a7b, #8134af, #515bd4, #ffff)',
     WebkitBackgroundClip: 'text',
@@ -52,7 +60,7 @@ const Payment = () => {
           Төлбөрөө хэрхэн төлөх вэ?
         </h2>
         <p className='text-[#959499] md:text-lg text-base'>
-          Та дансаар болон криптогоор төлбөрөө төлөх боломжтой.
+          Та дансаар болон <span className='text-[#b1e48a]'>АрдКойн</span> оор төлбөрөө төлөх боломжтой.
         </p>
       </div>
       <div className='py-10'>
@@ -61,8 +69,8 @@ const Payment = () => {
           <div className='mt-5 text-[#959499]'>
             <p>
               🏦 М Банк {' '}
-              <button 
-                onClick={() => copyToClipboard('8000441100')} 
+              <button
+                onClick={() => copyToClipboard('8000441100')}
                 className='text-[#62caaa] hover:underline focus:outline-none underline'
               >
                 8000441100
@@ -74,18 +82,47 @@ const Payment = () => {
             <p className='mt-2'>🆘 Төлбөрийн буцаалт хийгдэхгүй тул та сонголтоо зөв хийнэ үү.</p>
           </div>
         </div>
+
+        <div className='py-10'>
+          <h3 className='md:text-3xl text-xl'>Криптогоор төлөх:</h3>
+          <div className='mt-5 text-[#959499]'>
+            <p>
+              🏦  USDT TRC20: {' '}
+              <button
+                onClick={() => copyToClipboard('TWGJPuTPpTwxDxqkfxf2U4TKb8KswH5EaY')}
+                className='text-[#F6E96B] hover:underline focus:outline-none underline'
+              >
+                TWGJPuTPpTwxDxqkfxf2U4TKb8KswH5EaY
+              </button>
+              {' '}
+              тоот данс руу шилжүүлээрэй.
+            </p>
+            <p className='mt-2'>🆘 Төлбөрийн буцаалт хийгдэхгүй тул та сонголтоо зөв хийнэ үү.</p>
+          </div>
+        </div>
+
         <div className='mt-16'>
           <h3 className='md:text-3xl text-xl'>Бидэнтэй нэгдсэн танд баярлалаа.</h3>
           <div className='mt-5 text-[#959499]'>
             <p>💬 Та өөрийн шилжүүлсэн баримтаа Instagram хуудас руу илгээнэ үү.</p>
             <p className='mt-2'>
-              Instagram Page: <a 
-                href="https://www.instagram.com/avaadyaviiservice/" 
+              Instagram Page: <a
+                href="https://www.instagram.com/avaadyaviiservice/"
                 style={instagramLinkStyle}
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 https://www.instagram.com/intelliums/
+              </a>
+            </p>
+            <p className='mt-2'>
+              Facebook Page: <a
+                href="https://www.instagram.com/avaadyaviiservice/"
+                style={facebookLinkStyle}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://www.facebook.com/share/6kmHDSCeFDUdhCGi/?mibextid=LQQJ4d
               </a>
             </p>
             <p className='mt-2'>💁🏻‍♀️ Үйлчилгээний ажилтан шалгасны дараа тан руу холбоо барих болно.</p>
